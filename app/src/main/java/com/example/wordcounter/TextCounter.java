6f7b7bb3fbfd0a3f01efb4801416c48d8bc21faf
TomasKondrotas <tomaskond123@gmail.com>
2020-12-02 13:56:10 +0200

    Update TextCounter.java

--- app/src/main/java/com/example/wordcounter/TextCounter.java
+++ app/src/main/java/com/example/wordcounter/TextCounter.java
@@ -6,9 +6,16 @@ public class TextCounter {
     }
 
 
-    public static int countStrings(String text) {
-        return text.split("\\s+").length;
+    public static int countStrings(String text)
+     {
+       int dot_split;
+       int cama_split;
+
+          dot_split = text.split("\\.").length -1;
+          cama_split = text.split(",").length -1;
+          if(dot_split == -1) {dot_split = 0;}
+          if(cama_split == -1) {cama_split = 0;}
+      return text.split("\\s+").length + cama_split + dot_split ;
     }
 
 }
-
